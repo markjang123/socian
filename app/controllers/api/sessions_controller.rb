a920cacbf4fceccd3c19158aab890c34c1df7e02
Mark Jang <mark.jang@yahoo.com>
2020-10-13 14:05:39 -0700

    Add user auth methods in session controller and users controller

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,9 +1,16 @@
 class Api::SessionsController < ApplicationController
+    before_action :require_logged_in, only: [:destroy]
     def create
-
+        @user = User.find_by(params[:user][:username_or_email], params[:user][:password])
+        if @user
+            login!(@user)
+            render :show
+        else
+            render json: {errors: "Invalid credentials"}
+        end
     end
     
     def destroy
-
+        logout!
     end
 end
