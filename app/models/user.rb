ff6f6fdc7c76b43b16df3d65464327012ab33073
Mark Jang <mark.jang@yahoo.com>
2020-10-13 10:57:58 -0700

    Add user model SPIRE methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,32 @@
 class User < ApplicationRecord
+    validates :username, :email, :session_token, :password_digest, :password null: false
+    validates :username, :email, :session_token, uniqueness: true
+    validate :password, length: {minimum : 6}, allow_nil: true
+    after_initialize :ensure_session_token
+    attr_reader :password
+    def self.find_by_credentials(username, password)
+        @user = User.find_by(username: username)
+        if @user && self.is_password?(password)
+            @user
+        else 
+            nil
+        end
+    end
+
+    def password=(password)
+        self.password_digest = BCrypt::Password.create(password)
+        @password = password
+    end
+    
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)   
+    end
+
+    def reset_session_token!
+        self.session_token = SecureRandom::urlsafe_base64
+    end
+
+    def ensure_session_token
+        self.session_token ||= SecureRandom::urlsafe_base64
+    end
 end
