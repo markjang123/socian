c1e9c3330fb0b45ae030a83fbad906c713afb197
Mark Jang <mark.jang@yahoo.com>
2020-10-14 09:24:25 -0700

    Update entry point

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -3,10 +3,10 @@ import {Provider} from 'react-redux';
 import {HashRouter} from 'react-router-dom';
 import App from './App'
 
-const Root = ({store} => {
+const Root = ({store}) => {
     <Provider store={store}>
         <HashRouter>
             <App />
         </HashRouter>
     </Provider>
-})
+}
--- frontend/socian.jsx
+++ frontend/socian.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import LoginForm from './components/session/login_form'
 import configureStore from './store/store'
-const store = configureStore();
-window.dispatch = store.dispatch;
-window.getState = store.getState;
 
 document.addEventListener("DOMContentLoaded",() => {
     const root = document.getElementById("root")
+    const store = configureStore();
+    window.dispatch = store.dispatch;
+    window.getState = store.getState;
     ReactDOM.render(<LoginForm store={store}/>, root)
 })
