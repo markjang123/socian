2622d97a8fa749306a02ac2ad0a4e16f13daacf6
Mark Jang <mark.jang@yahoo.com>
2020-10-14 01:41:46 -0700

    Add session errors reducer

---
+++ frontend/reducers/session_errors_reducer.js
@@ -0,0 +1,14 @@
+import {RECEIVE_SESSION_ERRORS} from '../actions/session_actions'
+
+const ErrorsReducer = (oldState = {sessionErrors: []}, action) => {
+    Object.freeze(oldState)
+
+    switch(action.type){
+        case RECEIVE_SESSION_ERRORS:
+            return action.sessionErrors
+        default: 
+            return oldState
+    }
+}
+
+export default SessionReducer
