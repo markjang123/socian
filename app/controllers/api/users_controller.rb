a192595ee03f0b3e061eed5502011313fe0a3e78
Mark Jang <mark.jang@yahoo.com>
2020-10-13 21:39:34 -0700

    Add users show route and controller action

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -9,6 +9,7 @@ class Api::UsersController < ApplicationController
     end
     end
     def show
+        @user = User.find_by(id: params[:id])
         render :show
     end
 
---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,29 @@
+import * as SessionUtils from '../util/session_api_util'
+export const RECEIVE_CURRENT_USER_ID = 'RECEIVE_CURRENT_USER_ID';
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
+
+const receiveCurrentUserId = (userId) => {
+    return {
+        type: RECEIVE_CURRENT_USER_ID,
+        userId
+    }
+}
+const receiveSessionErrors = (sessionErrors) => {
+    return {
+        type: RECEIVE_SESSION_ERRORS,
+        sessionErrors
+    }
+}
+
+const signup = (user) => {
+    return dispatch => {
+       return SessionUtils.signup(user)
+                .then(response => dispatch(receiveCurrentUserId(response.id)))
+    }
+}
+const login = (user) => {
+    return dispatch => {
+       return SessionUtils.signup(user)
+                .then(response => dispatch(receiveCurrentUserId(response.id)))
+    }
+}
