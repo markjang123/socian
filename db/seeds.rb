66a0eb62da354c384f5276343a1eb9d187e51506
Mark Jang <mark.jang@yahoo.com>
2020-10-18 06:07:07 -0700

    Add demouser in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(username: "demoUser", password_digest: "$2a$12$JT98UKjUzhLIwE8Wbo3/EO.NRF43w9CT1b62c8ZSw7q6TR9uMIB8C", email: "demo@user.com", user_type: "fan")
