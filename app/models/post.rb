b030e4e3c231380823a1994ee1d75a53d838c5c5
Mark Jang <mark.jang@yahoo.com>
2020-10-19 09:42:59 -0700

    Fix syntax error in post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
-validates :author_id, :body, :title, presence: true
 class Post < ApplicationRecord
+    validates :author_id, :body, :title, presence: true
     belongs_to :author, 
         primary_key: :id,
         foreign_key: :author_id,
