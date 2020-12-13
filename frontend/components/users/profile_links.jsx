91a556acc9b1ceb7cac56573612aac56eb19b383
Mark Jang <mark.jang@yahoo.com>
2020-12-13 15:40:44 -0800

    Fix styling on user profile

--- frontend/components/users/profile_links.jsx
+++ frontend/components/users/profile_links.jsx
@@ -4,11 +4,11 @@ class ProfileLinks extends React.Component {
     render(){
         const {facebook_link, youtube_link, twitter_link, tumblr_link, soundcloud_link} = this.props.pageUser
         return (<ul className="profile-links">
-                    {facebook_link && <li className="profile-link"><a target="_blank" href={facebook_link}>Facebook</a></li>}
-                    {soundcloud_link && <li className="profile-link"><a target="_blank" href={soundcloud_link}>SoundCloud</a></li>}
-                    {twitter_link && <li className="profile-link"><a target="_blank" href={twitter_link}>Twitter</a></li>}
-                    {tumblr_link && <li className="profile-link"><a target="_blank" href={tumblr_link}>Tumblr</a></li>}
-                    {youtube_link && <li className="profile-link"><a target="_blank" href={youtube_link}>YouTube</a></li>}
+                    {facebook_link && <li><a className="profile-link" target="_blank" href={facebook_link}>Facebook</a></li>}
+                    {soundcloud_link && <li><a className="profile-link" target="_blank" href={soundcloud_link}>SoundCloud</a></li>}
+                    {twitter_link && <li><a className="profile-link" target="_blank" href={twitter_link}>Twitter</a></li>}
+                    {tumblr_link && <li><a className="profile-link" target="_blank" href={tumblr_link}>Tumblr</a></li>}
+                    {youtube_link && <li><a className="profile-link" target="_blank" href={youtube_link}>YouTube</a></li>}
                 </ul>)
     }
 }
